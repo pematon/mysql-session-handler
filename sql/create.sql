74097be8308961dad826759d5e1784279303a597
Marián Černý <marian@pematon.com>
2014-12-03 23:37:50 +0100

    Fix Year 2038 problem
    Do not use signed 32-bit integer for timestamp.

--- sql/create.sql
+++ sql/create.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `sessions`;
 
 CREATE TABLE `sessions` (
   `id` varchar(64) NOT NULL,
-  `timestamp` int(11) NOT NULL,
+  `timestamp` int unsigned NOT NULL,
   `data` longtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
