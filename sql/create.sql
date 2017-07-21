070f70241eee888d56f6cefed1850755026e000c
Marián Černý <marian@pematon.com>
2017-07-21 10:22:58 +0200

    Switch from utf8 to utf8mb4

--- sql/create.sql
+++ sql/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `sessions` (
   `id` char(32) CHARSET 'ascii' NOT NULL,
   `timestamp` int unsigned NOT NULL,
-  `data` longtext NOT NULL,
+  `data` longtext CHARSET 'utf8mb4' NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
